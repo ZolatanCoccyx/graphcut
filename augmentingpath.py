ec0585e6f3251b82c8feae23c3e5c5fa7799958d
Guillaume <guillaume.ballif@ens-cachan.fr>
2016-02-10 11:09:09 +0100

    version lente mais qui marche

--- augmentingpath.py
+++ augmentingpath.py
@@ -1,91 +1,217 @@
+a=np.matrix([[0.1,0.1,0.6],[0.1,0.6,0.6],[0.6,0.6,0.6]])<0.5
+
+def neighbours2(g,node):
+    m, n = g.dim
+    
+    if node == (-1,0):
+        return [(i, j) for i in range(m) for j in range(n)]
+        
+    if node == (0,-1):
+        return [(i, j) for i in range(m) for j in range(n)]
+    a, b = node
+    
+    if a == 0 :
+        if b == 0:
+            return [(a + 1, b), (a, b + 1), (0,-1)]
+        elif b == n-1:
+            return [(a + 1, b), (a, b - 1), (0,-1)]
+        else :
+            return [(a + 1, b), (a, b - 1), (a, b + 1), (0,-1)]
+            
+    elif a == m-1 :
+        if b == 0:
+            return [(a - 1, b), (a, b + 1), (0,-1)]
+        elif b == n-1:
+            return [(a - 1, b), (a, b - 1), (0,-1)]
+        else :
+            return [(a - 1, b), (a, b - 1), (a, b + 1), (0,-1)]
+            
+    elif b == 0 : 
+        return [(a - 1, b), (a, b + 1), (a + 1, b), (0,-1)]
+        
+    elif b == m-1 :
+        return [(a - 1, b), (a, b - 1), (a + 1, b), (0,-1)]
+    return [(a - 1, b), (a, b - 1), (a + 1, b), (a, b + 1), (0,-1)]
+
+def treeflow(g,a,b):
+    
+    if g(a)[2]==1:
+        return(g.flow(a,b))
+    elif g(a)[2]==2:
+        return(g.flow(b,a))
+    else:
+        return False
+
 def growth(g,A):
     while A:
         p=A.pop()
-        for q in neighbours(g,p):
-            if g.flow(p,q)!=0:
+        A.append(p)
+        # print(p)
+        for q in neighbours2(g,p):
+            if treeflow(g,p,q)>0:
                 if g(q)[2]==0:
                     g(q)[2]=g(p)[2]
                     g(q)[3],g(q)[4]=p
-                    A.append(q)
-                elif g(q)[2]!=g(p)[2]:
-                    P=path(g.root,g.term,g)
+                    if not (q in A):
+                        A.append(q)
+                        g(q)[1]=1
+                elif g(q)[2]!=0 and g(q)[2]!=g(p)[2]:
+                    P=path2(p,q,g)
                     return P
         A.remove(p)
+        g(p)[1]=0
     return []
 
+g=imgraph(a,5)
+    
+##
 def path(a,b,g):
     P=[b]
-    temp1,temp2=g(b)[3],g(b)[4]  
-    temp=(temp1,temp2)  
-    if temp and temp!=a:
-        temp1=temp[3]
-        temp2=temp[4]
+    if a==b:
+        return P
+    temp1,temp2=b
+    temp=(temp1,temp2)
+    while temp!=a:
+        temp1=g((temp1,temp2))[3]
+        temp2=g(temp)[4]
         temp=(temp1,temp2)
         P.append(temp)
-    if temp:
-        return P.reverse()
-    return False
+    P.reverse()
+    return P
 
+def path2(a,b,g):
+    R=[]
+    P=[]
+    Q=[]
+    if g(a)[2]==1:
+        P=path((-1,0),a,g)
+        Q=path((0,-1),b,g)
+        Q.reverse()
+        R=P+Q
+    else:
+        P=path((-1,0),b,g)
+        Q=path((0,-1),a,g)
+        Q.reverse()
+        R=P+Q
+    return R
+    
+
+
+##
 
 def augmentation(g,P):
+    O=[]
     n=len(P)
     K=[]
-    O=[]
-    delta=min([g.flow(P[j],P[j+1]) for j in range(n-1)])
+    delta=min([np.abs(g.flow(P[j],P[j+1])) for j in range(n-1)])
     for j in range(n-1):
         i1=P[j]
         i2=P[j+1]
-        print(i1,i2)
-        # k=g.flow(i1,i2)
-        # g.flow(i1,i2)=k-delta
+        k=g.flow(i1,i2)
+        if k<0:
+            setflow(i1,i2,k+delta)
+        elif k>=0:
+            setflow(i1,i2,k-delta)
         if g.flow(i1,i2)==0:
             K.append(j)
     for j in K:
         p=P[j]
         q=P[j+1]
-        if p[2]==q[2]:
+        if g(p)[2]==g(q)[2]:
             if g(p)[2]==1:
-                g(q)[3],g(q)[4]=False,False
+                g(q)[3],g(q)[4]=0.5,0.5
                 O.append(q)
-            if p[2]==2:
-                g(p)[3],g(p)[4]=False, False
+            elif g(p)[2]==2:
+                g(p)[3],g(p)[4]=0.5,0.5
                 O.append(p)
+    return(O)
 
-
-def adoption(g,O):
+def adoption(g,O,A):
     while O:
         p=O.pop()
-        process(g,p)
+        process(g,p,A,O)
 
-def process(g,p):
+def process(g,p,A,O):
     compt=0
-    L=neighbours(p)
-    L.remove(g.term)
-    L.append(g.root)
+    L=neighbours2(g,p)
+    L.append((-1,0))
     for j in L:
-        if g(j)[2]==g(p)[2] and g.flow(j,p)>0:
+        if g(j)[2]==g(p)[2] and treeflow(g,j,p)>0:
             g(p)[3],g(p)[4]=j
             compt=1
             break
     if compt==0:
         for q in L:
             if g(p)[2]==g(q)[2]:
-                if g.flow(q,p)>0:
+                if treeflow(g,q,p)>0:
                     A.append(q)
+                    g(q)[1]=1
                 if (g(q)[3],g(q)[4])==p:
                     O.append(q)
-                    g(q)[3],g(q)[4]=False, False
+                    g(q)[3],g(q)[4]=0.5,0.5
         g(p)[2]=0
-        A.remove(p)
+        g(p)[1]=0
+        if p in A:
+            A.remove(p)
+
+g=imgraph(a,5)
 
 def augmingpath(g):
+    m,n=g.dim
     g.root[1]=1
     g.term[1]=1
     g.root[2]=1
     g.term[2]=2
+    g.root[3:5]=[0.5,0.5]
+    g.term[3:5]=[0.5,0.5]
+    A=[(-1,0),(0,-1)]
+    g.tab[:,:,3]=np.zeros((m,n))+0.5
+    g.tab[:,:,4]=np.zeros((m,n))+0.5
+    O=[]
     while True:
-        grow(g,A)
+        P=growth(g,A)
+        # print(['fin augmentation'])
+        # input()
         if P==[]:
             break
-        augmentation(g,P)
-        adoption(g,O)
+        # print('debut augmentation')
+        O=augmentation(g,P)
+        # print([g.flow.root])
+        # print([g.flow.term])
+        # print([g.flow.lr])
+        # print([g.flow.tb])
+        # print([O,'orphan'])
+        # input()
+        adoption(g,O,A)
+        # print(['fin cycle'])
+    # print('resultats')
+    # print([g.flow.root])
+    # print([g.flow.term])
+    # print([g.flow.lr])
+    # print([g.flow.tb])
+    # print([g.tab[:,:,2]])
+    return(g.tab[:,:,2])
+
+# augmingpath(g)
+
+## test
+import pylab as plt
+import time
+import matplotlib.pyplot
+
+a=imread('lena.png')
+a=a[0:51,0:51,0]
+b=np.random.normal(0,1,a.shape)
+a=a+b*0.1
+a=a<0.5
+
+a[10,10]=1
+g=imgraph(a,5)
+
+t=time.time()
+T=augmingpath(g)
+print(time.time()-t)
+
+## test 2
+
+a=imread('a2.png')
--- graphes.py
+++ graphes.py
@@ -23,7 +23,7 @@ wn=4
 
 class flow:
     
-    def __init__(self, g, booltab, w01 = 1, w11 = 6, w00 = 6, wn = 4):
+    def __init__(self, g, booltab, w01 = 1, w11 = 8, w00 = 8, wn = 4):
             self.root = booltab * w11 + (1 - booltab) * w01
             self.term = booltab * w01 + (1 - booltab) * w00
             self.lr = np.ones(booltab.shape) * wn
@@ -31,16 +31,16 @@ class flow:
     
     def __call__(self, a, b):
         
-        if a == g.root:
+        if a == (-1,0):
             return self.root[b]
         
-        if b == g.term:
+        if b == (0,-1):
             return self.term[a]
         
-        if a == g.term:
+        if a == (0,-1):
             return - self.term[b]
         
-        if b == g.root:
+        if b == (-1,0):
             return - self.root[a]
         
         if axeadj(a, b) == "vert":
@@ -58,7 +58,7 @@ class flow:
 
 class imgraph:
     
-    def __init__(self, booltab, nblabel = 1, w01 = 1, w11 = 6, w00 = 6, wn = 4):
+    def __init__(self, booltab, nblabel = 1, w01 = 4, w11 = 8, w00 = 8, wn = 6):
         a, b, = booltab.shape
         self.dim = (a, b)
         self.tab = np.zeros((a, b, nblabel))
@@ -89,7 +89,6 @@ class imgraph:
                     return wn
             else:
                 return inf
-        
         self.w = w
         self.setflow = setflow
 
@@ -105,24 +104,33 @@ class imgraph:
             return (abs(a1 - a2) == 1 and abs(b1 - b2) == 0) or (abs(a1 - a2) == 0 and abs(b1 - b2) == 1)
         
         self.isadj = isadj
+    
+    def __call__(self,coor):
+        a,b=coor
+        if a==-1:
+            return g.root
+        if b==-1:
+            return g.term
+        else:
+            return g.tab[a,b,:]
+    
 
 def setflow(a, b, v):
                 
     f = g.flow
             
-    if a == g.root:
+    if a == (-1,0):
         f.root[b] = v
         return
             
-    if b == g.term:
+    if b == (0,-1):
         f.term[a] = v
         return
-            
-    if a == g.term:
+    if a == (0,-1):
         f.term[b] = - v
         return
             
-    if b == g.root:
+    if b == (-1,0):
         f.root[a] = - v
         return
             
@@ -145,176 +153,35 @@ def neighbours(graph, node):
     
     m, n = graph.dim
     
-    if node == graph.root:
+    if node == (-1,0):
         return [(i, j) for i in range(m) for j in range(n)]
         
-    if node == graph.term:
+    if node == (0,-1):
         return []
-    
     a, b = node
-    m, n = m - 2, n - 2
     
     if a == 0 :
         if b == 0:
-            return [(a + 1, b), (a, b + 1), graph.term]
-        elif b == n:
-            return [(a + 1, b), (a, b - 1), graph.term]
+            return [(a + 1, b), (a, b + 1), (0,-1)]
+        elif b == n-1:
+            return [(a + 1, b), (a, b - 1), (0,-1)]
         else :
-            return [(a + 1, b), (a, b - 1), (a, b + 1), graph.term]
+            return [(a + 1, b), (a, b - 1), (a, b + 1), (0,-1)]
             
-    elif a == m :
+    elif a == m-1 :
         if b == 0:
-            return [(a - 1, b), (a, b + 1), graph.term]
-        elif b == n:
-            return [(a - 1, b), (a, b - 1), graph.term]
+            return [(a - 1, b), (a, b + 1), (0,-1)]
+        elif b == n-1:
+            return [(a - 1, b), (a, b - 1), (0,-1)]
         else :
-            return [(a - 1, b), (a, b - 1), (a, b + 1), graph.term]
+            return [(a - 1, b), (a, b - 1), (a, b + 1), (0,-1)]
             
     elif b == 0 : 
-        return [(a - 1, b), (a, b + 1), (a + 1, b), graph.term]
+        return [(a - 1, b), (a, b + 1), (a + 1, b), (0,-1)]
         
     elif b == m :
-        return [(a - 1, b), (a, b - 1), (a + 1, b), graph.term]
-    return [(a - 1, b), (a, b - 1), (a + 1, b), (a, b + 1), graph.term]
+        return [(a - 1, b), (a, b - 1), (a + 1, b), (0,-1)]
+    return [(a - 1, b), (a, b - 1), (a + 1, b), (a, b + 1), (0,-1)]
 
 # a = imread("lena.png")[150:160,50:60] > 0.2
 # g = imgraph(a, 3)
-
-##Poussage de flot
-
-a=np.matrix([[0.6,0.4,0.4,0.9,0.9],[0.2,0.3,0.4,0.56,0.57],[0.1,0.4,0.9,0.8,0.9],[0.2,0.2,0.6,0.6,0.6],[0.1,0.1,0.6,0.6,0.6]])<0.5
-g=imgraph(a,5)
-
-def poussage(g):
-    n,m=g.dim
-    g.tab[:,:,1]=np.zeros((n,m))+1
-    g.root[1]=n*m
-    g.flow.root=np.zeros((n,m))
-    A=[(i,j) for i in range(n) for j in range(m)]
-    while len(A)>0:
-        i=A[0]
-        i1,i2=i
-        L=neighbours(g,i)
-        L.append(g.root)
-        e=sum([g.w(j,i)-np.abs(g.flow(i,j)) for j in L])
-        print(i,e)
-        while len(A)>0 and A[0]==i:
-            compt=0
-            if g.tab[i1,i2,1]==g.term[1]+1 and g.flow(i,g.term)>0:
-                k=g.flow(i,g.term)
-                setflow(i,g.term,k-min(k,e))
-                e=e-min(e,k)
-                compt=1
-                print([k,g.flow(i,g.term),e,g.term])
-            else:
-                L=neighbours(g,i)
-                L.remove(g.term)
-                for j in L:
-                    j1,j2=j
-                    if compt==0 and g.flow(i,j)>0 and g.tab[i1,i2,1]==g.tab[j1,j2,1]+1:
-                        k=g.flow(i,j)
-                        setflow(i,j,k-min(k,e))
-                        e=e-min(e,k)
-                        compt=1
-                        print(k,g.flow(i,j),e,j)
-            print([e,compt])
-            if e==0:
-                A.remove(i)
-            if compt==0:
-                L=neighbours(g,i)
-                L.remove(g.term)
-                k1=g.root[1]
-                k2=g.term[1]
-                for j in L:
-                    if g.flow(i,j)<=0:
-                        L.remove(j)
-                if g.flow(i,g.root)<=0:
-                    k1=float('infinity')
-                if g.flow(i,g.term)<=0:
-                    k2=float('infinity')
-                k=min([g.tab[j1,j2,1]+1 for (j1,j2) in L])
-                g.tab[i1,i2,1]=min(k,k1,k2)
-                print(g.tab[:,:,1])
-            # print(A)
-    return([g.flow.lr,g.flow.tb])
-
-t=time.time()
-K=poussage(g)
-print(time.time()-t)
-# lr=K[0]
-# tb=K[1]
-# Llr=lr[1:,:]-lr[:-1,:]
-# # Ltb=tb[1:,:]-tb[:-1,:]
-# # Clr=lr[:,1:]-lr[:,:-1]
-# Ctb=tb[:,1:]-tb[:,:-1]
-# Nlr=np.sqrt(Llr[:,:-1]**2+Ctb[:-1,:]**2)
-# print(time.time()-t)
-# imshow(Nlr,cmap=cm.gray)        
-
-
-
-
-
-## Augmenting path
-
-
-def growth(g, A):
-    '''Première coordonnée : étiquette, Seconde : actif si 1, Troisième : dans S si 1, Quatrième : première coordonnée parent, Cinquième : deuxième coordonnée parent '''
-    if g.term[2] == 1:
-        P = path(g.root, g.term, g.tab[:,:,3])
-    while A:
-        p = A.pop()
-        for q in neighbours(g, p):
-            if g.flow(p, q) != 0 and :
-                q[2] = 1
-                if q[1] = 0:
-                    q[1] = 1
-                    A.append(q)
-                    q[3] = p
-                if q = g.term:
-                    P = path(g.root, g.term, g.tab[:,:,3])
-        p[1] = 0
-        
-        # Augmentation #
-        
-        n = len(P)
-        argmin = 0
-        min = g.flow(P[0], P[1])
-        for k in range(n-1):
-            temp = g.flow(P[k], P[k+1])
-            if temp < min :
-                argmin = k
-                min = temp
-        for k in range(n-1):
-            g.setflow(P[k], P[k+1], g.flow(P[k], P[k+1]) - min)
-        orphans = []
-        for k in range(n-1):
-            if g.flow(P[k], P[k+1]) == 0:
-                P[k+1][3] = False
-                orphans.append(P[k+1])
-        
-        # Adoption #
-        
-        while orphans :
-            orphan = orphans.pop()
-            neighlist = neighbour(orphan)
-            for q in neighlist:
-                if g.flow(orphan, q) != 0 and q[2] == 1:
-                    if path(g.root, q, g):
-                        orphan[3] = q
-                    else:
-                        A.append(q)
-                        q[1] = 1
-                if not orphan[3]:
-                    orphan[2] = 0
-                    for q in neighlist:
-                        if q[3] = orphan:
-                            q[3] = False
-                            orphans.append(q)
-        
-    
-                    
-
-
-    
-    
